7cf80fd6ec03a4d86a19315c3017acf3fd19287b
Christina Landvytovych <Crofty.land@gmail.com>
2018-12-21 23:45:17 +0200

    connected video component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,13 +1,14 @@
 import React  from 'react';
 import { Player } from 'video-react';
 
+
 export default class Video extends React.Component{
     render() {
         return (
             <Player
                 playsInline
                 poster="/assets/poster.png"
-                src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+                src="https://www.youtube.com/embed/Ro51SuLyh8A" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen
             />
         );
     }
